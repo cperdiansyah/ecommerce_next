966a05494f09204c7deb62e595487ed119a7087a
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-06-21 08:55:27 +0700

    feat : add login and register page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,23 +1,110 @@
-import React from 'react';
+/* eslint-disable react/no-children-prop */
+import React, { useState } from 'react';
+import Link from 'next/link';
+import {
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  FormControl,
+  FormHelperText,
+  InputRightElement,
+} from '@chakra-ui/react';
 
-import Loader from '../src/components/Loader';
-import Message from '../src/components/Message';
 import Layout from '../src/parts/Layout';
 
-const login = () => {
+import { FaLock, FaAt } from 'react-icons/fa';
+
+const CFaLock = chakra(FaLock);
+const CFaaAt = chakra(FaAt);
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('login');
+  };
+
   return (
     <Layout>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message status='error' message={error} />
-      ) : (
-        <>
-          <h1>Login</h1>
-        </>
-      )}
+      <div className='form-wrapper w-[50%] mx-auto block'>
+        <form onSubmit={submitHandler}>
+          <h1 className='text-3xl font-sans py-5 text-center text-primary font-bold'>
+            Login
+          </h1>
+          <Stack
+            spacing={4}
+            p='1rem'
+            backgroundColor='whiteAlpha.900'
+            boxShadow='md'
+          >
+            <FormControl>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents='none'
+                  children={<CFaaAt color='gray.300' />}
+                />
+                <Input
+                  type='email'
+                  placeholder='Email address'
+                  autoComplete={false}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents='none'
+                  color='gray.300'
+                  children={<CFaLock color='gray.300' />}
+                />
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Password'
+                  autoComplete={false}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement width='4.5rem'>
+                  <Button h='1.75rem' size='sm' onClick={handleShowClick}>
+                    {showPassword ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <FormHelperText textAlign='right'>
+                <Link href='/forgot-password'>forgot password?</Link>
+              </FormHelperText>
+            </FormControl>
+            <Button
+              borderRadius={0}
+              type='submit'
+              variant='solid'
+              className='bg-primary text-white rounded-lg py-5 text-lg'
+              width='full'
+            >
+              Login
+            </Button>
+          </Stack>
+        </form>
+        <Box className='text-right mt-5'>
+          New to us?{' '}
+          <Link href='/register'>
+            <a className='text-primary'>Sign Up</a>
+          </Link>
+        </Box>
+      </div>
     </Layout>
   );
 };
 
-export default login;
+export default Login;
---
+++ pages/register.jsx
@@ -0,0 +1,129 @@
+/* eslint-disable react/no-children-prop */
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+import {
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  FormControl,
+  FormHelperText,
+  InputRightElement,
+} from '@chakra-ui/react';
+
+import Layout from '../src/parts/Layout';
+import { FaUserAlt, FaLock, FaAt } from 'react-icons/fa';
+
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
+const CFaaAt = chakra(FaAt);
+
+const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('login');
+  };
+
+  return (
+    <Layout>
+      <div className='form-wrapper w-[50%] mx-auto block'>
+        <form onSubmit={submitHandler}>
+          <h1 className='uppercase text-3xl font-sans py-5 text-center text-primary font-bold'>
+            Signup
+          </h1>
+          <Stack
+            spacing={4}
+            p='1rem'
+            backgroundColor='whiteAlpha.900'
+            boxShadow='md'
+          >
+            <FormControl>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents='none'
+                  children={<CFaUserAlt color='gray.300' />}
+                />
+                <Input
+                  type='text'
+                  placeholder='Fullname'
+                  autoComplete={false}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </InputGroup>
+            </FormControl>
+
+            <FormControl>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents='none'
+                  children={<CFaaAt color='gray.300' />}
+                />
+                <Input
+                  type='email'
+                  placeholder='email address'
+                  autoComplete={false}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </InputGroup>
+            </FormControl>
+
+            <FormControl>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents='none'
+                  color='gray.300'
+                  children={<CFaLock color='gray.300' />}
+                />
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Password'
+                  autoComplete={false}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement width='4.5rem'>
+                  <Button h='1.75rem' size='sm' onClick={handleShowClick}>
+                    {showPassword ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <FormHelperText textAlign='right'>
+                <Link href='/forgot-password'>forgot password?</Link>
+              </FormHelperText>
+            </FormControl>
+            <Button
+              borderRadius={0}
+              type='submit'
+              variant='solid'
+              className='bg-primary text-white rounded-lg py-5 text-lg'
+              width='full'
+            >
+              Login
+            </Button>
+          </Stack>
+        </form>
+        <Box className='text-right mt-5'>
+          Have an Account?{' '}
+          <Link href='/login'>
+            <a className='text-primary'>Login</a>
+          </Link>
+        </Box>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
