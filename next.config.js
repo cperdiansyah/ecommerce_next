c6734556b970cff51465cc6164012c82bb7a688b
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-06-28 11:21:06 +0700

    feat : remove env

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  /* env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-  }, */
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
